6cc49143c4dbd3938bd0896f540ac94b75409ee3
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-04-30 11:17:16 +0200

    AuthReducer added

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,17 +1,24 @@
 import {
   ActionReducer,
   ActionReducerMap,
-  createFeatureSelector,
+  createFeatureSelector, createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer, on
 } from '@ngrx/store';
+import { User } from '../model/user.model';
+import { AuthActions } from '../actions-types';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
+export interface AuthState {
+  user: User;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState = {
+  user: undefined
 };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => ({ ...state, user: action.user}))
+);
