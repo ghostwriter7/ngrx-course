34ab7313f2b40fb8e2b045a905421c49e064a355
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-05-02 13:50:21 +0200

    TimeTravelling debugger configured

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,3 +1,9 @@
+import { ActionReducerMap } from '@ngrx/store';
+import { routerReducer, SerializedRouterStateSnapshot } from '@ngrx/router-store';
+
 export interface AppState {
 }
 
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
+};
