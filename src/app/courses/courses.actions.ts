34306f5fe064940a7edbffc6219cb6fd249c6971
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-05-03 21:41:31 +0200

    Update Course action added

--- src/app/courses/courses.actions.ts
+++ src/app/courses/courses.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { Course } from './model/course';
+import { Update } from '@ngrx/entity';
 
 export const loadAllCourses = createAction(
   '[Courses Resolver] Load All Courses'
@@ -9,3 +10,9 @@ export const allCoursesLoaded = createAction(
   '[Load Courses Effect] All Courses Loaded',
   props<{ courses: Course[]}>()
 );
+
+export const courseUpdated = createAction(
+  '[Edit Course Dialog] Course updated',
+  props<{ update: Update<Course> }>()
+);
+
