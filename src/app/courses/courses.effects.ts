a5b8a063bfd0d01e29bf603c3527743c011f2348
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-05-06 19:56:37 +0200

    Saving course in the background added via the Effect

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -7,11 +7,16 @@ import { concatMap, map } from 'rxjs/operators';
 @Injectable()
 export class CoursesEffects {
 
-  loadAllCourses = createEffect(() => this.actions$.pipe(
+  loadAllCourses$ = createEffect(() => this.actions$.pipe(
     ofType(CourseActions.loadAllCourses),
     concatMap((action) => this.coursesService.findAllCourses()),
     map((courses) => CourseActions.allCoursesLoaded({courses}))
   ));
+
+  saveCourse$ = createEffect(() => this.actions$.pipe(
+    ofType(CourseActions.courseUpdated),
+    concatMap(action => this.coursesService.saveCourse(action.update.id, action.update.changes))
+  ), { dispatch: false });
 
   constructor(private actions$: Actions,
               private coursesService: CoursesHttpService) {
